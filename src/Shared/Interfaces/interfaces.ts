d9aa010b434fe977423292be53955d72d06c0928
Federico Tensi <federicotensi@gmail.com>
2023-07-10 19:19:31 +0200

    added interfaces and types

---
+++ src/Shared/Interfaces/interfaces.ts
@@ -0,0 +1,29 @@
+import { FuelType, RefuelingMode } from './enums';
+
+export interface MapPoint {
+  lat: number,
+  lng: number,
+}
+
+export interface SearchBaseCriteria {
+  fuelType?: FuelType,
+  refuelingMode?: RefuelingMode,
+  priceOrder?: 'asc' | 'desc',
+}
+
+export interface SearchByZoneCriteria extends SearchBaseCriteria {
+  points: MapPoint[],
+}
+
+export interface SearchByGeographicAreaCriteria extends SearchBaseCriteria {
+  province?: string
+  region: number
+  town?: string
+}
+
+export interface SearchByBrandCriteria extends SearchBaseCriteria {
+  province?: string
+  region?: number
+  brand?: string
+  queryText?: string
+}
