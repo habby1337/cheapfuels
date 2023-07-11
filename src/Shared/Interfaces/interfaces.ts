4209254951942bfdcc946a5ef64e29215d8c65a1
Federico Tensi <federicotensi@gmail.com>
2023-07-11 13:26:16 +0200

    created new interface for form input

--- src/Shared/Interfaces/interfaces.ts
+++ src/Shared/Interfaces/interfaces.ts
@@ -1,29 +1,36 @@
 import { FuelType, RefuelingMode } from './enums';
 
 export interface MapPoint {
-  lat: number,
-  lng: number,
+  lat: number;
+  lng: number;
 }
 
 export interface SearchBaseCriteria {
-  fuelType?: FuelType,
-  refuelingMode?: RefuelingMode,
-  priceOrder?: 'asc' | 'desc',
+  fuelType?: FuelType;
+  refuelingMode?: RefuelingMode;
+  priceOrder?: 'asc' | 'desc';
 }
 
 export interface SearchByZoneCriteria extends SearchBaseCriteria {
-  points: MapPoint[],
+  points: MapPoint[];
 }
 
 export interface SearchByGeographicAreaCriteria extends SearchBaseCriteria {
-  province?: string
-  region: number
-  town?: string
+  province?: string;
+  region: number;
+  town?: string;
 }
 
 export interface SearchByBrandCriteria extends SearchBaseCriteria {
-  province?: string
-  region?: number
-  brand?: string
-  queryText?: string
+  province?: string;
+  region?: number;
+  brand?: string;
+  queryText?: string;
+}
+
+// Form interfaces
+export interface FormInputs {
+  vehicleId: string;
+  distance: string;
+  priceOrder: 'asc' | 'desc';
 }
