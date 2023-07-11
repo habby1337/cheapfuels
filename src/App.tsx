b2b1a81f5dcee267172556b0b84c1acefe4374b4
Federico Tensi <federicotensi@gmail.com>
2023-07-11 12:27:55 +0200

    added dep and routes to the app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
-import Form from "./Components/Form/Form"
-import NavBar from "./Components/NavBar/NavBar"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/routes/Home';
+import MyCars from './Components/routes/MyCars';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 
-
-function App() {
-
+const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
-      <Form />
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+          errorElement={<ErrorPage />}
+        />
+        <Route
+          path='/mycars'
+          element={<MyCars />}
+          errorElement={<ErrorPage />}
+        />
+        <Route
+          path='*'
+          element={<ErrorPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-
-
-
-    </>
-  )
-}
-
-export default App
+export default App;
