aa5098b2300ddf5849fd0e413f17a5ca8fc090a8
Federico Tensi <federicotensi@gmail.com>
2023-07-11 12:34:00 +0200

    React router (#9)
    * added react router dependences
    
    * altered imports
    
    * created base components for route handling
    
    * changed the a anchor with navlink and added custom classes when link is active
    
    * Added a general error page
    
    * added dep and routes to the app file
    
    * added new word to workspace word check
    
    * removed unused import

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,30 +1,78 @@
-import ThemeHandler from "@/Services/ThemeHandler"
-import { LucideFuel } from "lucide-react"
-const NavBar = () => {
-  return (
+import ThemeHandler from '@/Services/ThemeHandler';
+import { LucideFuel } from 'lucide-react';
+import { NavLink } from 'react-router-dom';
 
-    <nav className="bg-slate-200  border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center">
-          <span className=" mr-3" ><LucideFuel /></span>
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cheap Fuels</span>
+interface ActiveClassesProps {
+  isActive: boolean;
+}
+
+const NavBar = () => {
+  const ActiveClasses = ({ isActive }: ActiveClassesProps) => {
+    return isActive
+      ? 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+      : 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
+  };
+  return (
+    <nav className='border-gray-200 bg-slate-200 dark:bg-gray-900'>
+      <div className='flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto'>
+        <a
+          href='/'
+          className='flex items-center'>
+          <span className='mr-3 '>
+            <LucideFuel />
+          </span>
+          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+            Cheap Fuels
+          </span>
         </a>
-        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+        <button
+          data-collapse-toggle='navbar-default'
+          type='button'
+          className='inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+          aria-controls='navbar-default'
+          aria-expanded='false'>
+          <span className='sr-only'>Open main menu</span>
+          <svg
+            className='w-5 h-5'
+            aria-hidden='true'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 17 14'>
+            <path
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M1 1h15M1 7h15M1 13h15'
+            />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col md:items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-slate-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div
+          className='hidden w-full md:block md:w-auto'
+          id='navbar-default'>
+          <ul className='flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:items-center md:p-0 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-slate-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Fuels Map</a>
+              <NavLink
+                to='/'
+                className={ActiveClasses}
+                aria-current='page'>
+                Fuels Map
+              </NavLink>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Your Cars</a>
+              <NavLink
+                to={'/myCars'}
+                className={ActiveClasses}>
+                Your Cars
+              </NavLink>
             </li>
             <li>
-              <a href="https://tensi.dev" target="_blank" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">//Dev</a>
+              <NavLink
+                to='https://tensi.dev'
+                target='_blank'
+                className={ActiveClasses}>
+                //Dev
+              </NavLink>
             </li>
 
             <li>
@@ -34,9 +82,7 @@ const NavBar = () => {
         </div>
       </div>
     </nav>
+  );
+};
 
-  )
-}
-
-
-export default NavBar
+export default NavBar;
