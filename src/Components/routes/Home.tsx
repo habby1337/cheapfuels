344e7d0c517bf6fb1896e7a1d594fa68ec809b13
Federico Tensi <federicotensi@gmail.com>
2023-07-13 16:35:11 +0200

    added mapcontainer component

--- src/Components/routes/Home.tsx
+++ src/Components/routes/Home.tsx
@@ -1,9 +1,11 @@
 import Form from '../Form/Form';
+import MapContainer from '../Map-leaflet/MapContainer';
 
 const Home = () => {
   return (
     <div className='container block h-screen p-0'>
       <Form />
+      <MapContainer />
     </div>
   );
 };
