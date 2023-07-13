35ce731e718b0a54181b50485095f9b390996410
Federico Tensi <federicotensi@gmail.com>
2023-07-13 16:34:23 +0200

    added map import and display

--- src/Components/Map-leaflet/MapContainer.tsx
+++ src/Components/Map-leaflet/MapContainer.tsx
@@ -1,5 +1,9 @@
+import Map from './Map';
 const MapContainer = () => {
   return (
+    <div className='container block px-0 mt-3 border-4 h-5/6 bg-slate-200 dark:bg-slate-900 rounded-xl backdrop-blur'>
+      <Map />
+    </div>
   );
 };
 
