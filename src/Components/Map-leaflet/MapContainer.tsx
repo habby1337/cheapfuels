dfce6450d480bda783b3b2224cf0bf18e93bf1a5
Federico Tensi <federicotensi@gmail.com>
2023-07-16 21:40:14 +0200

    removed boilerplate code

--- src/Components/Map-leaflet/MapContainer.tsx
+++ src/Components/Map-leaflet/MapContainer.tsx
@@ -1,33 +1,9 @@
 import { useEffect } from 'react';
 import Map from './Map';
 
-import { FuelsApiClient } from 'osservaprezzi-carburanti-node';
-import { FuelType, RefuelingMode } from '@/Shared/Interfaces/enums';
-import { SearchByZoneCriteria } from '@/Shared/Interfaces/interfaces';
-
 const MapContainer = () => {
-  const client = new FuelsApiClient(
-    'http://5.189.131.174:8443/https://carburanti.mise.gov.it/ospzApi/'
-  );
-
   useEffect(() => {
-    const searchCriteria: SearchByZoneCriteria = {
-      points: [
-        { lat: 42.32843626674558, lng: 12.188716303785915 },
-        { lat: 42.389322963743865, lng: 12.37136400886404 },
-        { lat: 42.31726730642802, lng: 12.44277514167654 },
-      ],
-      fuelType: FuelType.ALL,
-      refuelingMode: RefuelingMode.ALL,
-      priceOrder: 'asc',
-    };
-
-    client.search
-      .byZone(searchCriteria)
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-
-    console.log('expecting call to osservaprezzi');
+    console.log('MapContainer.tsx');
   }, []);
 
   return (
