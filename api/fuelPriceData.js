34bbfc6c48e5759ccc5e10459e181b12a972f6db
Federico Tensi <federicotensi@gmail.com>
2023-08-07 16:50:08 +0200

    added fuelprice endpoint

--- api/fuelPriceData.js
+++ api/fuelPriceData.js
@@ -1,15 +1,18 @@
-import { connectToDatabase } from './lib/mongodb.js';
+import { connectToDatabase } from '../lib/mongodb.js';
 
 export default async function handler(req, res) {
   const { database } = await connectToDatabase();
   const collection = database.collection(process.env.ATLAS_COLLECTION);
 
+  //get latest document
   const results = await collection
     .find({})
     .project({
-      _id: false,
+      _id: 0,
     })
-    .toArray();
+    .sort({ date: -1 })
+    .limit(1)
+    .next();
 
   res.status(200).json(results);
 }
