76f8b95136b834cd8417ba35b599a02345668124
Federico Tensi <federicotensi@gmail.com>
2023-08-08 17:28:20 +0200

    minor fixes and code refactoring

--- api/searchByZone.js
+++ api/searchByZone.js
@@ -1,16 +1,9 @@
 import { FuelsApiClient } from 'osservaprezzi-carburanti-node';
+import customAxios from '../lib/customAxios.js';
 
 import axios from 'axios';
 export default async function handler(req, res) {
   try {
-    const customHeaders = {
-      'Access-Control-Allow-Origin': '*',
-      origin: 'mad9rev3pgf4qwz-FNQ',
-    };
-
-    const customAxios = axios.create({
-      headers: customHeaders,
-    });
     const miseBackend = new FuelsApiClient(
       'http://5.189.131.174:8443/https://carburanti.mise.gov.it/ospzApi/',
       customAxios
@@ -31,12 +24,15 @@ export default async function handler(req, res) {
     await miseBackend.search
       .byZone(searchCriteria)
       .then((data) => {
+        console.warn('data', data);
         res.status(200).json(data);
       })
       .catch((error) => {
+        console.warn('error', error);
         res.status(500).json(error);
       });
   } catch (error) {
+    console.warn('error', error);
     res.status(500).json(error);
   }
 }
