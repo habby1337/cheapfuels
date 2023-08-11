d30ad5ff4f8bc3404abd1a3a9e9d7fed6e54cb57
Federico Tensi <federicotensi@gmail.com>
2023-08-11 16:49:18 +0200

    added a new mobile menu

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,88 +1,136 @@
-import ThemeHandler from '@/Services/ThemeHandler';
-import { LucideFuel } from 'lucide-react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+
+import { LucideFuel } from 'lucide-react';
+import ThemeHandler from '../../Services/ThemeHandler';
 
 interface ActiveClassesProps {
   isActive: boolean;
 }
 
-const NavBar = () => {
-  const ActiveClasses = ({ isActive }: ActiveClassesProps) => {
-    return isActive
-      ? 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
-      : 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent';
+const ActiveClasses = ({ isActive }: ActiveClassesProps) => {
+  const base = ' flex items-center px-2 py-2 text-base font-medium rounded-md';
+  return isActive
+    ? 'block py-2 pl-3 pr-4 bg-indigo-600 text-white  dark:text-white rounded-md ' +
+        base
+    : 'block py-2 pl-3 pr-4 hover:text-indigo-600' + base;
+};
+const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
   };
+
   return (
-    <nav className='mb-5 border-gray-200 bg-slate-200 dark:bg-gray-900'>
-      <div className='flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto'>
-        <a
-          href='/'
-          className='flex items-center'>
-          <span className='mr-3 '>
-            <LucideFuel />
-          </span>
-          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-            Cheap Fuels
-          </span>
-        </a>
-        <button
-          data-collapse-toggle='navbar-default'
-          type='button'
-          className='inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-          aria-controls='navbar-default'
-          aria-expanded='false'>
-          <span className='sr-only'>Open main menu</span>
-          <svg
-            className='w-5 h-5'
-            aria-hidden='true'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 17 14'>
-            <path
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M1 1h15M1 7h15M1 13h15'
+    <nav className='mb-5 bg-gray-200 border-gray-200 dark:text-white dark:bg-gray-900 text-slate-600'>
+      <div className='px-4 mx-auto max-w-7xl'>
+        <div className='flex items-center justify-between py-4 md:justify-start md:space-x-10'>
+          <div className='flex justify-start lg:w-0 lg:flex-1'>
+            <NavLink
+              to='/'
+              className='flex items-center'>
+              <LucideFuel className='w-6 h-6 mr-2' />
+              Cheap Fuels
+            </NavLink>
+          </div>
+          <div className='-my-2 -mr-2 md:hidden'>
+            <button
+              onClick={toggleMobileMenu}
+              type='button'
+              className='inline-flex items-center justify-center p-2 text-gray-400 bg-gray-200 border-gray-400 rounded-md dark:bg-gray-900 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+              <span className='sr-only'>Open menu</span>
+              {isMobileMenuOpen ? (
+                <svg
+                  className='w-6 h-6'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  aria-hidden='true'>
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M6 18L18 6M6 6l12 12'
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className='w-6 h-6'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                  aria-hidden='true'>
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M4 6h16M4 12h16M4 18h16'
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div className='items-center hidden space-x-6 md:flex place-content-evenly'>
+            <NavLink
+              to='/'
+              className={ActiveClasses}
+              aria-current='page'>
+              Fuels Map
+            </NavLink>
+            <NavLink
+              to='/myCars'
+              className={ActiveClasses}>
+              Your Cars
+            </NavLink>
+            <NavLink
+              to='https://tensi.dev'
+              target='_blank'
+              className={ActiveClasses}
+              rel='noopener noreferrer'>
+              <span className='font-mono'>//Dev</span>
+            </NavLink>
+            <ThemeHandler
+              iconSize={20}
+              className='w-[70px] h-[30px] items-baseline '
             />
-          </svg>
-        </button>
-        <div
-          className='hidden w-full md:block md:w-auto'
-          id='navbar-default'>
-          <ul className='flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:items-center md:p-0 bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-slate-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-            <li>
-              <NavLink
-                to='/'
-                className={ActiveClasses}
-                aria-current='page'>
-                Fuels Map
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={'/myCars'}
-                className={ActiveClasses}>
-                Your Cars
-              </NavLink>
-            </li>
-            <li>
+          </div>
+        </div>
+      </div>
+      {isMobileMenuOpen && (
+        <div className='mt-2 space-x-2 md:hidden'>
+          <div className='items-start pb-4 '>
+            <NavLink
+              to='/'
+              className={ActiveClasses}>
+              Fuels Map
+            </NavLink>
+            <NavLink
+              to='/myCars'
+              className={ActiveClasses}>
+              Your Cars
+            </NavLink>
+
+            <div className='flex items-center justify-between mt-2 '>
               <NavLink
                 to='https://tensi.dev'
                 target='_blank'
+                rel='noopener noreferrer'
                 className={ActiveClasses}>
-                //Dev
+                <span className='font-mono'>//Dev</span>
               </NavLink>
-            </li>
-
-            <li>
-              <ThemeHandler iconSize={20} />
-            </li>
-          </ul>
+              <ThemeHandler
+                iconSize={20}
+                className='w-[70px] h-[30px]'
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </nav>
   );
 };
 
-export default NavBar;
+export default Navbar;
